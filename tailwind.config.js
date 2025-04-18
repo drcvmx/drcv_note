496a160a0c1b4318d9d1070ee4d69b0075aa54c9
Dante Chavez <drcv.work.code@gmail.com>
2025-04-18 00:58:56 -0600

    upload tailwild

--- tailwind.config.js
+++ tailwind.config.js
@@ -78,5 +78,9 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require('@tailwindcss/typography') // Agrega esta línea
+  ],
+  
 }
